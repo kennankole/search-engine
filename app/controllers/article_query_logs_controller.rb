87fcf9759471a971cb825748032759455f523c8c
kennankole <kennankole@gmail.com>
2024-05-21 12:50:05 +0300

    Writing integration test for article_query_logs_controller

--- app/controllers/article_query_logs_controller.rb
+++ app/controllers/article_query_logs_controller.rb
@@ -11,28 +11,19 @@ class ArticleQueryLogsController < ApplicationController
       }, status: :created
     else
       render json: {
-        errors: search_log.errors.full_messages
+        errors: @search_log.errors.full_messages
       }, status: :unprocessable_entity
     end
   end
 
   def index
-    # @search_logs = ArticleQueryLog.all
     @search_trends = ArticleQueryLog.all
+    render json: @search_trends
   end
 
   private
 
   def search_params
     params.require(:search_log).permit(:query)
-  end
-
-  def summarize_searches
-    logs = ArticleQueryLog.order(:ip, :created_at).pluck(:ip, :query)
-    grouped_searches = logs.group_by { |ip, _| ip }
-    summarized_searches = grouped_searches.transform_values do |queries|
-      queries.map(&:last).chunk_while { |i, j| i != j }.map(&:last).uniq
-    end
-    summarized_searches.values.flatten.tally
   end
 end
--- spec/requests/article_query_logs_spec.rb
+++ spec/requests/article_query_logs_spec.rb
@@ -1,18 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe "ArticleQueryLogs", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/article_query_logs/create"
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    context 'with valid parameters' do
+      let(:valid_attributes) { { search_log: { query: 'Testing search engine' } } }
+
+      it 'creates a new ArticleQueryLog' do
+        expect {
+          post '/article_query_logs', params: valid_attributes
+        }.to change(ArticleQueryLog, :count).by(1)
+      end
+
+      it 'returns a success response' do
+        post article_query_logs_path, params: valid_attributes
+        expect(response).to have_http_status(:created)
+        expect(JSON.parse(response.body)['status']).to eq('Success')
+      end
+    end
+
+    context 'with invalid parameters' do
+      let(:invalid_attributes) { { search_log: { query: '' } } }
+      it 'does not create a new ArticleQueryLog' do
+        post article_query_logs_path, params: invalid_attributes
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)['errors']).to eq(["Query can't be blank"])
+      end
     end
   end
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/article_query_logs/index"
-      expect(response).to have_http_status(:success)
+  describe 'GET #index' do
+    it 'returns a success response' do
+      get article_query_logs_index_path
+      expect(response).to be_successful
+    end
+
+    it 'assigns all search logs to @search_trends' do
+      ArticleQueryLog.create!(query: 'First query', ip: '127.0.0.1')
+      ArticleQueryLog.create!(query: 'Second query', ip: '127.0.0.1')
+      ArticleQueryLog.create!(query: 'Third query', ip: '127.0.0.1')
+
+      get article_query_logs_index_path
+      json_response = JSON.parse(response.body)
+      expect(json_response.size).to eq(3)
     end
   end
-
 end
